6653f3430f7b73d75584ba5f0dc5062aa77932a5
Jack Wilde <jack@lightsonsoftware.com>
2024-04-22 15:51:55 -0400

    checkpoint, build all versions

--- build_docs.py
+++ build_docs.py
@@ -1,7 +1,24 @@
+# build all versions of the documentation
+
+import os
 import subprocess
+import yaml
 
-def build_docs():
-    subprocess.run(["cd","core", "&&", "sphinx-build", "-b", "html", "docs", "docs/_build"])
-    print("Docs built successfully")
+def build_doc(project, version):
+    print(f"Building {project} {version}")
+    os.environ[f"{project}_version"] = version
+    subprocess.run(f"cd {project}/{version} && make html", shell=True)
 
-build_docs()
+def move_dir(src, dst):
+    subprocess.run(["mkdir", "-p", dst])
+    subprocess.run("mv "+src+'* ' + dst, shell=True)
+
+os.environ["pages_root"] = "https://tenstorrent.github.io/docs-test/"
+
+with open("versions.yml", "r") as yaml_file:
+    docs = yaml.safe_load(yaml_file)
+    for project, versions in docs.items():
+        for version in versions:
+            print(f"Building {project} {type(versions)}")
+            build_doc(project, version)
+            move_dir(f"{project}/{version}/_build/html/", f"docs/{project}/{version}/")
