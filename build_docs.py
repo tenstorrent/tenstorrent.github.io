bbce2fe1e82a400622b26c00337736c6014bf2ed
Jack Wilde <jack@lightsonsoftware.com>
2024-04-25 14:35:04 -0400

    build sccript for all doc versions

--- build_docs.py
+++ build_docs.py
@@ -7,7 +7,11 @@ import yaml
 def build_doc(project, version):
     print(f"Building {project} {version}")
     os.environ[f"{project}_version"] = version
-    subprocess.run(f"cd {project}/{version} && make html", shell=True)
+    subprocess.run("git checkout " + version, shell=True)
+    # subprocess.run("git checkout main -- conf.py", shell=True)
+    # subprocess.run("git checkout main -- versions.yaml", shell=True)
+
+    subprocess.run(f"cd {project} && make html", shell=True)
 
 def move_dir(src, dst):
     subprocess.run(["mkdir", "-p", dst])
@@ -19,6 +23,9 @@ with open("versions.yml", "r") as yaml_file:
     docs = yaml.safe_load(yaml_file)
     for project, versions in docs.items():
         for version in versions:
-            print(f"Building {project} {type(versions)}")
+            print(f"Building {project} {version}")
             build_doc(project, version)
-            move_dir(f"{project}/{version}/_build/html/", f"docs/{project}/{version}/")
+            move_dir(f"{project}/_build/html/", f"output/{project}/{version}/")
+            print(f"Built {project} {version}")
+    
+    subprocess.run("git checkout jwilde/test-pybuda-version", shell=True)
