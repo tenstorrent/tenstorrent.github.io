0d9da0c35d29dfafb1693c9f1ac3dc2f2e00df7f
Dimitri Gnidash <dimitri@tenstorrent.com>
2024-10-02 14:26:49 -0400

    Update the latest version to v0.52.0 and support the addition of the structured tags

--- build_docs.py
+++ build_docs.py
@@ -11,16 +11,16 @@ def build_doc(project, version, additional_cmd):
         return
     
     subprocess.run(f"git checkout {project}_{version}", shell=True)
-    subprocess.run("git checkout dimitri/build_process_for_multiple_versions -- versions.yml", shell=True)
-    subprocess.run("git checkout dimitri/build_process_for_multiple_versions -- tt-metalium/conf.py", shell=True)
-    subprocess.run("git checkout dimitri/build_process_for_multiple_versions -- ttnn/conf.py", shell=True)
+    subprocess.run("git checkout main -- versions.yml", shell=True)
+    subprocess.run("git checkout main -- tt-metalium/conf.py", shell=True)
+    subprocess.run("git checkout main -- ttnn/conf.py", shell=True)
 
     version_no_v = version.replace("v", "")
 
     command = f"python3 -m venv .{version} && source .{version}/bin/activate\n"
     
     if additional_cmd:
-        command += additional_cmd
+        command += additional_cmd + "\n"
 
     command += f"cd {project} && make html\n"
     command += "deactivate\n"
